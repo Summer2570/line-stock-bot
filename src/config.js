994bf076fae87b1252cb1402946dc2c1def1b730
phattharin marasan <somwan2547@gmail.com>
2026-05-13 08:46:32 +0700

    fix line env names

--- src/config.js
+++ src/config.js
@@ -10,9 +10,9 @@ export const config = {
 
   line: {
     channelAccessToken:
-      process.env.CHANNEL_ACCESS_TOKEN,
+      process.env.LINE_CHANNEL_ACCESS_TOKEN,
 
     channelSecret:
-      process.env.CHANNEL_SECRET
+      process.env.LINE_CHANNEL_SECRET
   }
 };
